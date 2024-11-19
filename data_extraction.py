aa2bcfef78e2ca3dad4898ea84d7dba56a3954af
Yiu410 <cyshihaa@connect.ust.hk>
2024-11-19 15:11:06 +0800

    seperate data from main function

---
+++ data_extraction.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+from bokeh.models import ColumnDataSource , NumeralTickFormatter
+from bokeh.plotting import figure
+
+from bokeh.palettes import Category10, Category20
+
+
+def get_sales_data():
+    
+    sales_df = pd.read_csv("dataset/Sales_table.csv")
+
+    volvo_sales = sales_df.loc[sales_df['Maker'] ==
+                            'VOLVO', ["2016", "2017", "2018", "2019", "2020"]]
+
+    toyota_sales = sales_df.loc[sales_df['Maker'] ==
+                                'TOYOTA', ["2016", "2017", "2018", "2019", "2020"]]
+
+    nissan_sales = sales_df.loc[sales_df['Maker'] ==
+                                'NISSAN', ["2016", "2017", "2018", "2019", "2020"]]
+
+    volkswagen_sales = sales_df.loc[sales_df['Maker'] ==
+                                    'VOLKSWAGEN', ["2016", "2017", "2018", "2019", "2020"]]
+
+    total_volvo_sales = volvo_sales.sum(axis=0).values
+    total_toyota_sales = toyota_sales.sum(axis=0).values
+    total_nissan_sales = nissan_sales.sum(axis=0).values
+    total_volkswagen_sales = volkswagen_sales.sum(axis=0).values
+
+    return total_volvo_sales, total_toyota_sales, total_nissan_sales, total_volkswagen_sales 
+
+def main_page_setup():
+    #extract the data
+    total_volvo_sales, total_toyota_sales, total_nissan_sales, total_volkswagen_sales = get_sales_data()
+    years = np.arange(2016, 2021)
+    data = pd.DataFrame({'Year': years, 'VOLVO': total_volvo_sales,
+                        'TOYOTA': total_toyota_sales, 'NISSAN': total_nissan_sales, 'VOLKSWAGEN': total_volkswagen_sales})
+
+    source = ColumnDataSource(data)
+
+    main_page = figure(title="Automotive Market Dynamics Visualization", x_axis_label='Year',
+            y_axis_label='Sales', sizing_mode='stretch_both', width=800, height=400)
+
+    colors = Category20[4]
+    colors_label = ['Volvo', 'Toyota', 'Nissan', 'Volkswagen']
+    main_page.varea_stack(stackers=['VOLVO', 'TOYOTA', 'NISSAN', 'VOLKSWAGEN'],
+                x='Year', color=colors, source=source, legend_label=colors_label)
+    
+
+    main_page.yaxis.formatter = NumeralTickFormatter(format="0,0")
+    main_page.title.text_font_size = '20pt'
+    main_page.legend.location = "top_left"
+    main_page.legend.orientation = "horizontal"
+
+    return main_page
--- main.py
+++ main.py
@@ -7,60 +7,29 @@ from bokeh.palettes import Category10, Category20
 import numpy as np
 import pandas as pd
 
-sales_df = pd.read_csv("dataset/Sales_table.csv")
+from data_extraction import get_sales_data, main_page_setup
 
-volvo_sales = sales_df.loc[sales_df['Maker'] ==
-                           'VOLVO', ["2016", "2017", "2018", "2019", "2020"]]
+#extract the data
+total_volvo_sales, total_toyota_sales, total_nissan_sales, total_volkswagen_sales = get_sales_data()
 
-toyota_sales = sales_df.loc[sales_df['Maker'] ==
-                            'TOYOTA', ["2016", "2017", "2018", "2019", "2020"]]
+main_page = main_page_setup()
 
-nissan_sales = sales_df.loc[sales_df['Maker'] ==
-                            'NISSAN', ["2016", "2017", "2018", "2019", "2020"]]
-
-volkswagen_sales = sales_df.loc[sales_df['Maker'] ==
-                                'VOLKSWAGEN', ["2016", "2017", "2018", "2019", "2020"]]
-
-total_volvo_sales = volvo_sales.sum(axis=0).values
-total_toyota_sales = toyota_sales.sum(axis=0).values
-total_nissan_sales = nissan_sales.sum(axis=0).values
-total_volkswagen_sales = nissan_sales.sum(axis=0).values
-
-years = np.arange(2016, 2021)
-
-data = pd.DataFrame({'Year': years, 'VOLVO': total_volvo_sales,
-                    'TOYOTA': total_toyota_sales, 'NISSAN': total_nissan_sales, 'VOLKSWAGEN': total_volkswagen_sales})
-print(data.head())
-
-source = ColumnDataSource(data)
-
-p = figure(title="Automotive Market Dynamics Visualization", x_axis_label='Year',
-           y_axis_label='Sales', sizing_mode='stretch_both', width=800, height=400)
-
-colors = Category20[4]
-colors_label = ['Volvo', 'Toyota', 'Nissan', 'Volkswagen']
-p.varea_stack(stackers=['VOLVO', 'TOYOTA', 'NISSAN', 'VOLKSWAGEN'],
-              x='Year', color=colors, source=source, legend_label=colors_label)
-
+#show vertical lines with cursor:
 vline = Span(location=0, dimension = 'height',line_color='black', line_width =2 , line_dash = [10,5])
-p.add_layout(vline)
-p.js_on_event('mousemove', CustomJS(args=dict(vline=vline), code="""
+main_page.add_layout(vline)
+main_page.js_on_event('mousemove', CustomJS(args=dict(vline=vline), code="""
     const x = cb_obj.x;
     vline.location = x;
 """))
 
+#show information when hover
 hover = HoverTool()
 hover.tooltips = [("Year", "@Year"), ("Volvo",
                                       "@{VOLVO}{0,0}"), ('Toyota', "@{TOYOTA}{0,0}"), ("Nissan", "@{VOLVO}{0,0}"), ("Volkswagen", "@{VOLKSWAGEN}{0,0}")]
-p.add_tools(hover)
+main_page.add_tools(hover)
 
-p.yaxis.formatter = NumeralTickFormatter(format="0,0")
-p.title.text_font_size = '20pt'
-p.legend.location = "top_left"
-p.legend.orientation = "horizontal"
-
-l = layout([[p]], sizing_mode='stretch_both')
-curdoc().add_root(l)
+final_layout = layout([[main_page]], sizing_mode='stretch_both')
+curdoc().add_root(final_layout)
 
 output_file("automotive_market_dynamics.html")
-show(l)
+show(final_layout)
